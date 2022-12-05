9bc2b528a4591b81bd792bbb41d89b068371bbac
wpaq <wallysonpabloo@gmail.com>
2022-12-05 18:49:33 -0300

    test: ensure EmailService throws MissingParamError if no receiverName is provided

--- src/services/email.service.spec.js
+++ src/services/email.service.spec.js
@@ -1,17 +1,26 @@
 const { MissingParamError } = require('../utils/errors')
 
 class EmailService {
-  async send(email) {
-    if (!email) {
-      throw new MissingParamError('email')
+  async send(receiverName, receiverEmail) {
+    if (!receiverName) {
+      throw new MissingParamError('receiverName')
+    }
+    if (!receiverEmail) {
+      throw new MissingParamError('receiverEmail')
     }
   }
 }
 
 describe('Email Service', () => {
-  test('Should throw if no email is provided', async () => {
+  test('Should throw if no receiverName is provided', async () => {
     const sut = new EmailService()
     const promise = sut.send()
-    expect(promise).rejects.toThrow(new MissingParamError('email'))
+    expect(promise).rejects.toThrow(new MissingParamError('receiverName'))
+  })
+
+  test('Should throw if no receiverEmail is provided', async () => {
+    const sut = new EmailService()
+    const promise = sut.send('any_receiverName')
+    expect(promise).rejects.toThrow(new MissingParamError('receiverEmail'))
   })
 })
